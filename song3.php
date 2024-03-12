61c54074634cbc500749392c1c5b40a59215c3ab
FuerteSeannWilliam <seannfuerte7@gmail.com>
2024-03-12 18:04:35 +0800

    Puth Tunes

---
+++ song3.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+<head>
+    <title>Song 3</title>
+    
+<head>
+<body style = "text-align:center;">
+    <?php
+    echo "<h1>Dangerously</h1>";
+    echo "by Charlie Puth<br>";
+    echo "Lyrics and Guitar Chords<br>";
+    echo "     <br>";
+    
+    echo"[Intro]<br>";
+    echo"Dm Am7 Bb<br>";
+    echo"Dm Am7 Bb<br>";
+
+    
+    echo"[Verse 1]<br>";
+
+    echo"Dm                       Am7                  Bb<br>";
+    echo"This is gonna hurt but I blame myself first, cause I ignored the truth<br>";
+    echo"Dm                                   Bb<br>";
+    echo"Drunk off that love, my head up, there's no forgetting you<br>";
+
+
+    echo"[Pre-chorus]<br>";
+
+    echo"        Dm                   Am7               Bb<br>";
+    echo"You've awoken me, but you're choking me, I was so obsessed<br>";
+    echo"         Dm                 Am7               Bb<br>";
+    echo"Gave you all of me, and now honestly I've got nothing left<br>";
+
+
+    echo"[Chorus]<br>";
+
+    echo"                     Bb       F       C<br>";
+    echo"I loved you dangerously-y-y-y-y-y-y-y-y<br>";
+    echo"Dm            C          Bb            F      Aaug<br>";
+    echo"More than the air that I breathe-e-e-e-e-e-e-e-e<br>";
+    echo"                           Bb                 F<br>";
+    echo"Knew we would crash at the speed that we were going<br>";
+    echo"       C             Dm   C         Bb       F      Aaug<br>";
+    echo"Didn't care if the explosion ruined me-e-e-e-e-e-e-e-e<br>";
+    echo"                          Dm<br>";
+    echo"Baby I loved you dangerously<br>";
+    echo"Am7   Bb<br>";
+    echo"Mmmm, mmm<br>";
+    echo"                     Dm Am7 Bb<br>";
+    echo"I loved you dangerously-y oooh<br>";
+    
+     
+    echo"[Verse 2]<br>";
+    echo"Dm                 Am7                Bb<br>";
+    echo"Usually I hold the power with both my hands tied behind my back<br>";
+    echo"Dm                               Am7                       Bb<br>";
+    echo"Look at how things change, cause now you're the train, and I'm tied to the track<br>";
+
+
+    echo"[Pre-chorus]<br>";
+
+    echo"        Dm                Am7               Bb<br>";
+    echo"You've awoken, but you're choking me, I was so obsessed<br>";
+    echo"         Dm                 Am7               Bb<br>";
+    echo"Gave you all of me, and now honestly I've got nothing left<br>";
+
+
+    echo"[Chorus]<br>";
+
+    echo"                           Bb       F       C<br>";
+    echo"Cause I loved you dangerously-y-y-y-y-y-y-y-y<br>";
+    echo"Dm            C          Bb            F      Aaug<br>";
+    echo"More than the air that I breathe-e-e-e-e-e-e-e-e<br>";
+    echo"                           Bb                 F<br>";
+    echo"Knew we would crash at the speed that we were going<br>";
+    echo"       C             Dm   C         Bb       F      Aauh<br>";
+    echo"Didn't care if the explosion ruined me-e-e-e-e-e-e-e-e<br>";
+    echo"                           Gm<br>";
+    echo" Baby I loved you dangerously<br>";
+
+
+    echo"[Bridge]<br>";
+
+    echo"                        Dm7<br>";
+    echo"You took me down, down, down, down <br>";
+    echo"C                           Gm<br>";
+    echo"And kissed my lips with goodbye<br>";
+    echo"                    F<br>";
+    echo"I see you now, now, now, now<br>";
+    echo"C/E                Gm<br>";
+    echo"It was a matter of time<br>";
+    echo"            Dm7           C                         Bb<br>";
+    echo"You know, I know, there's only one place this could lead<br>";
+    echo"                      Asus4<br>";
+    echo"But you are the fire, I'm gasoline<br>";
+
+
+    echo"[Chorus 2]<br>";
+
+    echo"N.C                                                      Bb       F       C<br>";
+    echo"I love you, I love you, I love you, I loved you dangerously-y-y-y-y-y-y-y-y<br>";
+    echo"Dm            C          Bb            F      Aaug<br>";
+    echo"More than the air that I breathe-e-e-e-e-e-e-e-e<br>";
+    echo"                           Bb                 F<br>";
+    echo"Knew we would crash at the speed that we were going<br>";
+    echo"       C             Dm   C         Bb       F      Aaug<br>";
+    echo"Didn't care if the explosion ruined me-e-e-e-e-e-e-e-e<br>";
+    echo"                          Dm<br>";
+    echo"Baby I loved you dangerously<br>";
+
+
+    echo"[Outro]<br>";
+
+    echo"Am7  Bb<br>";
+    echo"Mmm, mmm<br>";
+    echo"                         Dm7 Am7<br>";
+    echo"Oh, I loved you dangerously<br>";
+    echo"    Bb<br>";
+    echo"Oh, oh, I loved you dangerously<br>";
+   
+    ?>
+</body>
