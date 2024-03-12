dd1e756c1a49c4fc797cbc9a1475b9a476b2b88f
FuerteSeannWilliam <seannfuerte7@gmail.com>
2024-03-12 18:01:35 +0800

    Puth Tunes

---
+++ song2.php
@@ -0,0 +1,185 @@
+<!DOCTYPE html>
+<head>
+    <title>Song 2</title>
+    
+<head>
+<body style = "text-align:center;">
+    <?php
+    echo "<h1>Light Switch</h1>";
+    echo "by Charlie Puth<br>";
+    echo "Lyrics and Guitar Chords<br>";
+    echo "     <br>";
+
+
+    echo"[Intro]<br>";
+    echo"Bm  D  A  Em<br>";
+    echo"              Yeah<br>";
+
+
+    echo"[Verse 1]<br>";
+
+    echo"Bm                        D<br>";
+    echo"Why you callin' at eleven-thirty<br>";
+    echo"                           A<br>";
+    echo"When you only wanna do me dirty? (Uh-uh, uh-uh)<br>";
+    echo"      Em<br>";
+    echo"But I hit right back<br>";
+    echo"                           Bm<br>";
+    echo"'Cause you got that-that, yeah<br>";
+    echo"                               D<br>";
+    echo"Why you always wanna act like lovers?<br>";
+    echo"                             A<br>";
+    echo"But you never wanna be each others'";
+    echo"(Uh-uh, uh-uh)<br>";
+    echo"       Em<br>";
+    echo"I say don't look back<br>";
+    echo"But I go right back, yeah<br>";
+
+
+    echo"[Pre-chorus]<br>";
+
+    echo"Bm<br>";
+    echo"All of a sudden, I'm hypnotized<br>";
+    echo"D<br>";
+    echo"You're the one that I can't deny<br>";
+    echo"A                                Em<br>";
+    echo"Every time that I say I'm gonna walk away<br>";
+    
+    
+    echo"[Chorus]<br>";
+    echo"            Bm<br>";
+    echo"You turn me on like a switch<br>";
+    echo"                         D<br>";
+    echo"When you're movin' your body around and around<br>";
+    echo"     A<br>";
+    echo"Now, I don't wanna fight this (No)<br>";
+    echo"                      Em<br>";
+    echo"You know how to just make me want<br>";
+    echo"            Bm<br>";
+    echo"You turn me on like a light switch<br>";
+    echo"                          D<br>";
+    echo"When you're movin' your body around and around<br>";
+    echo"     A<br>";
+    echo"You got me in a tigth grip (Yeah)<br>";
+    echo"                      Em<br>";
+    echo"You know how to just make me want you,<br>";
+    echo" Bm<br>";
+    echo"baby<br>";
+
+
+    echo"[Verse 2]<br>";
+
+    echo"                                D<br>";
+    echo"Do you love it when you keep me guessin'? (Me guessin')<br>";
+    echo"                                        A<br>";
+    echo"When you're leaving then you leave me stressin' (Me stressin')<br>";
+    echo"(Uh-uh, uh-uh) (Uh-uh)<br>";
+    echo"       Em<br>";
+    echo"But I can't say mad when you walk like that, no<br>";
+    echo"Bm                            D<br>";
+    echo"Why you always wanna act like lovers?<br>";
+    echo"                             A<br>";
+    echo"But you never wanna be each others'<br>";
+    echo"(Uh-uh, uh-uh) (Uh-uh)<br>";
+    echo"       Em<br>";
+    echo"I say don't look back<br>";
+    echo"But I go right back, yeah<br>";
+
+
+    echo"[Pre-chorus]<br>";
+    echo"Bm<br>";
+    echo"All of a sudden, I'm hypnotized (Hypnotized)<br>";
+    echo"D<br>";
+    echo"You're the one that I can't deny (Can't deny)<br>";
+    echo"A                                Em<br>";
+    echo"Every time that I say I'm gonna walk away (Yeah)<br>";
+
+    
+    echo"[Chorus<br>";
+    echo"            Bm<br>"; 
+    echo"You turn me on like a light switch<br>";
+    echo"                         D<br>";
+    echo"When you're movin' your body around and around<br>";
+    echo"    A<br>";
+    echo"Now I don't wanna fight this (No)<br>";
+    echo"                      Em<br>";
+    echo"You know how to just make me want <br>";
+    echo"You turn me on like a light switch (Switch)<br>";
+    echo"                          D<br>";
+    echo"When you're movin' your body around and around (Oh)<br>";
+    echo"     A<br>";
+    echo"You got me in a tight grip (Grip)<br>";
+    echo"                     Em<br>";
+    echo"You know how to just make me want you,<br>";
+    echo"Bm<br>";
+    echo"baby<br>";
+
+    
+    echo"[Post-chorus]<br>";
+    echo"                                 D<br>";
+    echo"Come on, come on, come on, come on,<br>";
+    echo"                                    A<br>";
+    echo"come on and show me how you do (You do)<br>";
+    echo"                                  Em<br>";
+    echo"You want, you want, you want, you want, you wanna keep me wantin' you<br>";
+    echo"         Bm<br>";
+    echo"(You're wantin' me girl)<br>";
+    echo"                                 D<br>";
+    echo"Come on, come on, come on, come on,<br>";
+    echo"                                  A<br>";
+    echo"come on and (Hey) show me how you do (How you do)<br>";
+    echo"                                   Em<br>";
+    echo"You want, you want, you want, you want<br>";
+    echo"(Yeah, yeah), you wanna keep me wantin' you<br.";
+
+
+    echo"[Bridge]<br>";
+    echo"            Bm<br>";
+    echo"You turn me on like a light switch<br>";
+    echo"                         D<br>";
+    echo"When you're movin' your body around and around<br>";
+    echo"    A<br>";
+    echo"Now I don't wanna fight this<br>";
+    echo"                      Em<br>";
+    echo"You know how to just make me want to, uhh<br>";
+
+
+    echo"[Chorus]<br>";
+
+    echo"            Bm<br>";
+    echo"You turn me on like a light switch<br>";
+    echo"                          D<br>";
+    echo"When you're movin' your body around and around<br>";
+    echo"     A<br>";
+    echo"You got me in a tight grip<br>";
+    echo"                      Em<br>";
+    echo"You know how to just make me want you,<br>";
+    echo"Bm<br>";
+    echo"baby<br>";
+    
+
+    echo"[Post-chorus]<br>";
+
+    echo"                                 D<br>";
+    echo"Come on, come on, come on, come on,<br>";
+    echo"come on and (Oh) show me how you do<br>";
+    echo"          A<br>";
+    echo"(How you do)<br>";
+    echo"                                   Em<br>";
+    echo"You want, you want, you want, you want<br>";
+    echo"                             Bm<br>";
+    echo"(Wanna keep me wantin' you, baby), you wanna keep me wantin' you<br>";
+    echo"                                D<br>";
+    echo"Come on, come on, come on, come on,<br>";
+    echo"                                         A<br>";
+    echo"come on and show me how you do (How you do)<br>";
+    echo"                                  Em<br>";
+    echo"You want, you want, you want, you want<br>";
+    echo"                          Bm<br>";
+    echo"you wanna keep me wantin' you<br>";
+    
+    
+   
+        
+    ?>
+</body>
