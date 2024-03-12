142b9b36b7fbd55db4e2330f79a8e804394520b9
FuerteSeannWilliam <seannfuerte7@gmail.com>
2024-03-12 17:57:23 +0800

    Puth Tunes

---
+++ song1.php
@@ -0,0 +1,126 @@
+<!DOCTYPE html>
+<head>
+    <title>Song 1</title>
+    
+<head>
+<body style = "text-align:center;">
+    <?php
+    echo "<h1>One Call Away</h1>";
+    echo "by Charlie Puth<br>";
+    echo "Lyrics and Guitar Chords<br>";
+    echo "     <br>";
+
+    echo"[Intro]<br>";
+
+    echo"         C   G      Am7<br>";
+    echo"I'm only one call away<br>";
+    echo"                 F    C    G<br>";
+    echo"I'll be there to save the day<br>";
+    echo"             C  G        Am7<br>";
+    echo"Superman got nothing on me<br>";
+    echo"         F   F/G    C<br>"; 
+    echo"I'm only one call away<br>";
+
+
+    echo"[Verse 1]<br>";
+
+    echo"Am        G            C      F <br>";
+    echo"Call me, baby, if you need a friend<br>";
+    echo"Am      G              C<br>";
+    echo"I just wanna give you love<br>";
+    echo"F               Am<br>";
+    echo"C'mon, c'mon, c'mon<br>";
+    echo"         G              C       F<br>";
+    echo"Reaching out to you, so take a chance<br>";
+
+
+    echo"[Pre-chorus]<br>";
+
+    echo"   F<br>";
+    echo"No matter where you go<br>";
+    echo"    G<br>";
+    echo"You know you're not alone<br>";
+
+
+    echo"[Chorus]<br>";
+
+    echo"         C   G      Am7<br>";
+    echo"I'm only one call away<br>";
+    echo"                 F    C    G<br>";
+    echo"I'll be there to save the day<br>";
+    echo"             C  G        Am7<br>";
+    echo"Superman got nothing on me<br>";
+    echo"         F   F/G    C<br>";
+    echo"I'm only one call away<br>";
+    
+
+    echo"[Verse 2]<br>";
+
+    echo"       G                C        F<br>";
+    echo"Come along with me and don't be scared<br>";
+    echo"        G             C<br>";
+    echo"I just wanna set you free<br>";
+    echo"  F             Am<br>";
+    echo"C'mon, c'mon, c'mon<br>";
+    echo"        F               C   F<br>";
+    echo"You and me can make it anywhere<br>";
+    echo"        Am           G               C   F<br>";
+    echo"But for now, we can stay here for a while<br>";
+    echo"          Am       G             C    F<br>";
+    echo"Cause you know, I wanna see you smile<br>";
+
+
+    echo"[Pre-chorus]<br>";
+
+    echo"   F<br>";
+    echo"No matter where you go<br>";
+    echo"    G<br>";
+    echo"You know you're not alone<br>";
+
+
+    echo"[Chorus]<br>";
+    echo"         C   G      Am7<br>";
+    echo"I'm only one call away<br>";
+    echo"                 F    C    G<br>";
+    echo"I'll be there to save the day<br>";
+    echo"             C  G        Am7<br>";
+    echo"Superman got nothing on me<br>";
+    echo"         F   F/G    C<br>";
+    echo"I'm only one call away<br>";
+    
+
+    echo"[Bridge]<br>";
+
+    echo"                F    Am        G<br>";
+    echo"And when you're weak I'll be strong<br>";
+    echo"          F    Am       G<br>";
+    echo"I'm gonna keep holding on<br>";
+    echo"              F         Am        G<br>";
+    echo"Now don't you worry, it won't be long<br>";
+    echo"                      F<br>";
+    echo"Darling, and when you feel like hope is gone<br>";
+    echo"     G<br>";
+    echo"Just run into my arms<br>";
+
+
+    echo"[Chorus]<br>";
+
+    echo"         C   G      Am7<br>";
+    echo"I'm only one call away<br>";
+    echo"                 F    C    G<br>";
+    echo"I'll be there to save the day<br>";
+    echo"             C  E7      Am7<br>";
+    echo"Superman got nothing on me<br>";
+    echo"         F                   F/G C   G      Am7<br>";
+    echo"I'm only one, I'm only o-o-o-o - one call away<br>";
+    echo"                 F    C    G<br>";
+    echo"I'll be there to save the day<br>";
+    echo"             C  G        Am7<br>";
+    echo"Superman got nothing on me<br>";
+    echo"         F   F/G    C<br>";
+    echo"I'm only one call away<br>";
+    echo"         F   F/G    C<br>";
+    echo"I'm only one call away<br>";
+    
+    ?>
+</body>
