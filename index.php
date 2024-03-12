f191a48bb7da84e2a604e827f1538ed779e7a54f
FuerteSeannWilliam <seannfuerte7@gmail.com>
2024-03-12 17:53:09 +0800

    index.php

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<head>
+    <title>Song 2</title>
+    <style>
+        a
+        {
+        color:black;
+        text-decoration: none;
+        font-size: 25px;
+        }
+    </style>
+
+<head>
+<body style = "text-align:center;">
+    <h1>Puth Tunes</h1>
+    <a href = song1.php> One Call Away</a><br>
+    <a href = song2.php> Light Switch</a><br>
+    <a href = song3.php> Dangerously</a><br>
+    <a href = song4.php> No More Drama </a><br>
+    <a href = song5.php> Done For Me</a><br>
+    
+
+</body>
