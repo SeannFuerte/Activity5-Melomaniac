00e3afd7f3826d7e04845d43e627ae630d01e879
FuerteSeannWilliam <seannfuerte7@gmail.com>
2024-03-12 18:12:06 +0800

    Puth Tunes

---
+++ song5.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<head>
+    <title>Song 5</title>
+    
+<head>
+<body style = "text-align:center;">
+    <?php
+    echo "<h1>Done For Me</h1>";
+    echo "by Charlie Puth ft. Kehlani<br>";
+    echo "Lyrics and Guitar Chords<br>";
+    echo "(Capo on 2nd Fret)<br>";
+    echo "     <br>";
+    echo "<pre>";
+    
+    
+    echo "[Intro - Am  G  F  E]<br>";
+    echo "Oh, oh<br>";
+    echo "Am<br>";
+    echo "What you thinking<br>";
+    echo "G                       F                  E<br>";
+    echo "You think that we could be better off with somebody new<br>";
+    echo "(No oh, oh baby, no)<br>";
+    echo "Am<br>";
+    echo "You say you're leaving<br>";
+    echo "G                        F                 E<br>";
+    echo "Well if you wanna leave there ain't nobody stopping you<br>";
+    echo "(No oh, oh baby, no)<br>";
+
+
+    echo "[Pre-chorus]<br>";
+
+    echo "Am<br>";
+    echo "I won't beg for your love<br>";
+    echo "G<br>";
+    echo "Won't say please<br>";
+    echo "F                          E<br>";
+    echo "I won't fall to the ground on my knees<br>";
+    echo "Am                       G<br>";
+    echo "You know I've given this everything<br>";
+    echo "     F              E<br>";
+    echo "Baby honestly, baby honestly<br>";
+    echo "<br>";
+    
+    echo "[Chorus]<br>";
+    echo "Am<br>";
+    echo "I lie for you baby<br>";
+    echo "G<br>";
+    echo "I die for you baby<br>";
+    echo "F<br>";
+    echo "Cry for you baby<br>";
+    echo "     E                         Am<br>";
+    echo "But tell me what you've done for me<br>";
+    echo "For you baby<br>";
+    echo "G<br>";
+    echo "And only you baby<br>";
+    echo "F<br>";
+    echo "The things I do baby<br>";
+    echo "     E                          Am<br>";
+    echo "But tell me what you've done for me<br>";
+    echo "<br>";
+    
+    echo "[Verse]<br>";
+    echo "Am<br>";
+    echo "I never cheated<br>";
+    echo "G       F                           E<br>";
+    echo "Deleted everyone cause they made you uncomfortable<br>";
+    echo "(No oh, oh baby, no)<br>";
+
+
+    echo "[Pre-chorus]<br>";
+    echo "Am<br>";
+    echo "I won't beg for your love<br>";
+    echo "G<br>";
+    echo "Won't say please (Oh no, won't say please)<br>";
+    echo "F                          E<br>";
+    echo "I won't fall to the ground on my knees (on my knees)<br>";
+    echo "Am                       G<br>";
+    echo "You know I've given this everything<br>";
+    echo "     F<br>";
+    echo "Baby honestly, (baby honestly),<br>";
+    echo "     E<br>";
+    echo "Baby honestly<br>";
+    echo "<br>";
+    
+    echo "[Chorus]<br>";
+    echo "Am<br>";
+    echo "I lie for you baby<br>";
+    echo "G<br>";
+    echo "I die for you baby<br>";
+    echo "F<br>";
+    echo "Cry for you baby<br>";
+    echo "     E                         Am<br>";
+    echo "But tell me what you've done for me<br>";
+    echo "For you baby<br>";
+    echo "G<br>";
+    echo "And only you baby<br>";
+    echo "F<br>";
+    echo "The things I do baby<br>";
+    echo "     E<br>                      Am";
+    echo "But tell me what you've done for me<br> ";
+    echo "<br>";
+    
+
+    echo "[Outro]<br>";
+    echo "Am<br>";
+    echo "(Tell me what you've done for me<br>";
+    echo "G<br>";
+    echo "Oh, tell me what you've done for me<br>";
+    echo "F<br>";
+    echo "(Tell me what you've done for me)<br>";
+    echo "E<br>";
+    echo "Tell me what you've done for me<br>";
+    echo "<br>";
+    
+    
+    ?>
+</body>
