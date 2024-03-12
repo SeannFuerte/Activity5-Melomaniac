48ecf991055285fee5d92f7288e51f478e9f9f1c
FuerteSeannWilliam <seannfuerte7@gmail.com>
2024-03-12 18:09:05 +0800

    Puth Tunes

---
+++ song4.php
@@ -0,0 +1,144 @@
+<!DOCTYPE html>
+<head>
+    <title>Song 4</title>
+    
+<head>
+<body style = "text-align:center;">
+    <?php
+    echo "<h1>No More Drama</h1>";
+    echo "by Charlie Puth <br>";
+    echo "Lyrics and Guitar Chords<br>";
+    echo "     <br>";
+    echo "<pre>";
+    
+    echo"[Intro]";
+    
+    echo"Em7  GM7  Bm  A<br>";
+
+
+    echo "[Chorus]<br>";
+    echo "   Em7<br>";
+    echo "Oh, I got no more drama in my life<br>";
+    echo "Gm7<br>";
+    echo "..And it's such a blessing<br>";
+    echo "Bm<br>";
+    echo "I'm so glad I finally realized<br>";
+    echo "     A<br>";
+    echo "I'm better without you<br>";
+    echo "Em7<br>";
+    echo "I got no more drama in my life<br>";
+    echo "GM7<br>";
+    echo "..And it's been amazing<br>";
+    echo "Bm<br>";
+    echo "I'm so glad I finally realized<br>";
+    echo "     A<br>";
+    echo "I'm better without you<br>";
+    
+    echo "[Verse 1]<br>";
+    echo "Em7                         GM7<br>";
+    echo "Took a year before I recognized<br>";
+    echo "                           Bm<br>";
+    echo "That our love had already died<br>";
+    echo "                              A<br>";
+    echo "Baby, I was down bad, I was down bad<br>";
+    echo "                 Em7<br>";
+    echo "Now I'm healing (Yeah)<br>";
+    echo "                            GM7<br>";
+    echo "Some nights, you would stay here<br>";
+    echo "                            Bm<br>";
+    echo "Some night, you would disappear<br>";
+    echo "                                 Ab<br>";
+    echo "Now I'll never come back, never come back<br>";
+    echo "To that feeling<br>";
+    
+    
+    echo "[Pre-chorus]<br>";
+    echo "     Em7<br>";
+    echo "Baby, I ain't even wondering<br>";
+    echo "       GM7<br>";
+    echo "About everything we could've been<br>";
+    echo "      Bm<br>";
+    echo "Ever since you looked me in my eyes,<br>";
+    echo "          A<br>";
+    echo "turned around<br>";
+    echo "And said goodbye<br>";
+    echo "<br>";
+    
+    echo "[Chorus]<br>";
+    echo "   Em7<br>";
+    echo "Oh, I got no more drama in my life<br>";
+    echo "GM7<br>";
+    echo "..And it's such a blessing<br>";
+    echo "Bm<br>";
+    echo "I'm so glad I finally realized<br>";
+    echo "     A<br>";
+    echo "I'm better without you<br>";
+    echo "Em7<br>";
+    echo "I got no more drama in my life<br>";
+    echo "GM7<br>";
+    echo "..And it's been amazing<br>";
+    echo "Bm<br>";
+    echo "I'm so glad I finally realized<br>";
+    echo "     A<br>";
+    echo "I'm better without you<br>";
+    echo "<br>";
+    
+    echo "[Verse 2]<br>";
+    echo "Em7                             GM7br>";
+    echo "You start fights while we're up late<br>";
+    echo "                        Bm<br>";
+    echo "Use your lips to manipulate<br>";
+    echo "                                    A<br>";
+    echo "Baby, how'd you do that? How'd you do that?<br>";
+    echo "Make me need it";
+    echo "Em7                    GM7<br>";
+    echo "Some nights, you were 'Baby'<br>";
+    echo "                      Bm<br>";
+    echo "Some nights, it was crazy<br>";
+    echo "                                  A<br>";
+    echo "Now I'll never come back, never come back<br>";
+    echo "To that feeling<br>";
+    echo "<br>";
+    
+    echo "[Pre-chorus]<br>";
+    echo "     Em7<br>";
+    echo "Baby, I ain't even wondering<br>";
+    echo "       GM7<br>";
+    echo "About everything we could've been<br>";
+    echo "      Bm<br>";
+    echo "Ever since you looked me in my eyes,<br>";
+    echo "          A<br>";
+    echo "turned around<br>";
+    echo "And said goodbye<br>";
+    echo "<br>";
+    
+    echo "[Chorus]<br>";
+    echo "   Em7<br>";
+    echo "Oh, I got no more drama in my life<br>";
+    echo "GM7<br>";
+    echo "..And it's such a blessing<br>";
+    echo "Bm<br>.";
+    echo "I'm so glad I finally realized<br>";
+    echo "     A<br>";
+    echo "I'm better without you<br>";
+    echo "Em7<br>";
+    echo "I got no more drama in my life<br>";
+    echo "GM7<br>";
+    echo ".. And it's been amazing<br>";
+    echo "Bm<br>";
+    echo "I'm so glad I finally realized<br>";
+    echo "     A<br>";
+    echo "I'm better without you<br>";
+    echo "<br>";
+
+    echo "[Outro]<br>";
+    echo "Em7     GM7<br>";
+    echo "..Oh-oh.....<br>";
+    echo "  Bm      A<br>";
+    echo "Alone, mm-mm<br>";
+    echo "<br>";
+
+    
+    echo "</pre>";
+    ?>
+</body>
